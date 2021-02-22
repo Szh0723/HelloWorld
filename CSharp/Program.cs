847b1fa108c7411d8999e9c52f5f5a8c7ca79bd3
Szh0723 <79350335+Szh0723@users.noreply.github.com>
2021-02-22 10:14:28 +0800

    Create Program.cs
    Commit to account to prevent account termination.

---
+++ CSharp/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Tests {
+  public static class Program {
+    public static int Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      return 0;
+    }
+  }
+}
